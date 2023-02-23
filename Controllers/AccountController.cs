f60f6ed0f71bc87de7ccc98d6258233b387a1e92
Ramadhani <ramadhanidwin@gmail.com>
2023-02-23 13:58:00 +0700

    Menambahkan Fungsi Login Pada Account

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -22,7 +22,7 @@ namespace MVC75NET.Controllers
 
         public IActionResult Index()
         {
-            var accounts = accRepository.GetAll();
+            var accounts = accRepository.GetEmployeeAccount();
             return View(accounts);
         }
 
@@ -45,6 +45,14 @@ namespace MVC75NET.Controllers
             var accounts = accRepository.GetById(id);
             return View(accounts);
         }
+
+        //Method Get Untuk login 
+        public IActionResult Login()
+        {
+
+            return View(); ;
+        }
+
 
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -107,97 +115,21 @@ namespace MVC75NET.Controllers
         {
             if (ModelState.IsValid)
             {
-                // Bikin kondisi untuk mengecek apakah data university sudah ada
-                University university = new University 
+                var result = accRepository.Register(registerVM);
+                if (result >0)
                 {
-                    Name = registerVM.UniversityName
-                };
-                if (context.Universities.Any(u => u.Name == registerVM.UniversityName))
-                {
-                    university.Id = context.Universities.FirstOrDefault(u => u.Name == university.Name).Id;
+                    return RedirectToAction("Index", "Home");
                 }
-                else
-                {    
-                  context.Universities.Add(university);
-                  context.SaveChanges();
-                }
-
-                Education education = new Education
-                {
-                    Major = registerVM.Major,
-                    Degree = registerVM.Degree,
-                    Gpa = registerVM.GPA,
-                    UniversityId = university.Id
-                };
-                context.Educations.Add(education);
-                context.SaveChanges();
-
-                Employee employee = new Employee
-                {
-                    NIK = registerVM.NIK,
-                    FirstName = registerVM.FirstName,
-                    LastName = registerVM.LastName,
-                    BirthDate = registerVM.BirthDate,
-                    Gender = (Models.GenderEnum)registerVM.Gender,
-                    HiringDate = registerVM.HiringDate,
-                    Email = registerVM.Email,
-                    PhoneNumber = registerVM.PhoneNumber,
-                };
-                context.Employees.Add(employee);
-                context.SaveChanges();
-
-                Account account = new Account
-                {
-                    EmployeeNIK = registerVM.NIK,
-                    Password = registerVM.Password
-                };
-                context.Accounts.Add(account);
-                context.SaveChanges();
-
-                AccountRole accountRole = new AccountRole
-                {
-                    AccountNIK = registerVM.NIK,
-                    RoleId = 2
-                };
-
-                context.AccountRoles.Add(accountRole);
-                context.SaveChanges();
-
-                Profiling profiling = new Profiling
-                {
-                    EmployeeId = registerVM.NIK,
-                    EducationId = education.Id
-                };
-                context.Profilings.Add(profiling);
-                context.SaveChanges();
-
-                return RedirectToAction("Index", "Home");
             }
             return View();
         }
 
-
-        //Method Get Untuk login 
-        public IActionResult Login()
-        {
-
-            return View(); ;
-        }
         //Method Post Untuk Login 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Login(LoginVM loginVM)
         {
-            var tampil = context.Accounts.Join(
-                context.Employees,
-                a => a.EmployeeNIK,
-                e => e.NIK,
-                (a, e) => new LoginVM
-                {
-                    Email = e.Email,
-                    Password = a.Password,
-                });
-            if (tampil.Any(e => e.Email == loginVM.Email && e.Password == loginVM.Password))
+            if (accRepository.Login(loginVM)) //Secara default if akan membandingkan nilai dengan true 
             {
                 return RedirectToAction("Index", "Home");
             }
--- Repositories/AccountRepository.cs
+++ Repositories/AccountRepository.cs
@@ -56,5 +56,105 @@ public class AccountRepository : iRepository<string, Account>
         return result;
     }
 
-    
+    public bool Login(LoginVM loginVM)
+    {
+        var getAccounts = context.Employees.Join(
+            context.Accounts,
+            e => e.NIK,
+            a => a.EmployeeNIK,
+            (e, a) => new LoginVM
+            {
+                Email = e.Email,
+                Password = a.Password
+            });
+
+        return getAccounts.Any(e => e.Email == loginVM.Email && e.Password == loginVM.Password);
+    }
+
+    public int Register(RegisterVM registerVM)
+    {
+        int result = 0;
+        University university = new University
+        {
+            Name = registerVM.UniversityName
+        };
+
+        // Bikin kondisi untuk mengecek apakah data university sudah ada
+        if (context.Universities.Any(u => u.Name == university.Name))
+        {
+            university.Id = context.Universities
+                .FirstOrDefault(u => u.Name == university.Name)
+                .Id;
+        }
+        else
+        {
+            context.Universities.Add(university);
+            result = context.SaveChanges();
+        }
+
+        Education education = new Education
+        {
+            Major = registerVM.Major,
+            Degree = registerVM.Degree,
+            Gpa = registerVM.GPA,
+            UniversityId = university.Id
+        };
+        context.Educations.Add(education);
+        result = context.SaveChanges();
+
+        Employee employee = new Employee
+        {
+            NIK = registerVM.NIK,
+            FirstName = registerVM.FirstName,
+            LastName = registerVM.LastName,
+            BirthDate = registerVM.BirthDate,
+            Gender = (Models.GenderEnum)registerVM.Gender,
+            HiringDate = registerVM.HiringDate,
+            Email = registerVM.Email,
+            PhoneNumber = registerVM.PhoneNumber,
+        };
+        context.Employees.Add(employee);
+        result = context.SaveChanges();
+
+        Account account = new Account
+        {
+            EmployeeNIK = registerVM.NIK,
+            Password = registerVM.Password
+        };
+        context.Accounts.Add(account);
+        result = context.SaveChanges();
+
+        AccountRole accountRole = new AccountRole
+        {
+            AccountNIK = registerVM.NIK,
+            RoleId = 2
+        };
+
+        context.AccountRoles.Add(accountRole);
+        result = context.SaveChanges();
+
+        Profiling profiling = new Profiling
+        {
+            EmployeeId = registerVM.NIK,
+            EducationId = education.Id
+        };
+        context.Profilings.Add(profiling);
+        result = context.SaveChanges();
+
+        return result;
+    }
+
+    public List<AccountEmployeeVM> GetEmployeeAccount()
+    {
+        var results = (from a in GetAll()
+                       join e in empRepository.GetAll()
+                       on a.EmployeeNIK equals e.NIK
+                       select new AccountEmployeeVM
+                       {
+                            Email = e.Email,
+                            Password = a.Password
+                       }).ToList();
+        return results;
+    }
+
 }
